f5f941ed04e391bc96f45b21ba4abfe7c4845981
Ueno Souza Silva <uenosouzasilva@gmail.com>
2023-11-15 18:24:51 -0300

    feat: criado o projeto ds-games

---
+++ src/main/java/com/dsgames/projeto/DsGamesApplication.java
@@ -0,0 +1,13 @@
+package com.dsgames.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsGamesApplication.class, args);
+	}
+
+}
